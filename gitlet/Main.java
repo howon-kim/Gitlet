854b125610abdbc7497dd4c433ee68b27f1465cd
Howon Kim <howonkim@me.com>
2019-12-01 20:40:11 -0800

    Move from WSL to Windows

---
+++ gitlet/Main.java
@@ -0,0 +1,19 @@
+package gitlet;
+
+/** Driver class for Gitlet, the tiny stupid version-control system.
+ *  @author
+ */
+public class Main {
+
+    /** Usage: java gitlet.Main ARGS, where ARGS contains
+     *  <COMMAND> <OPERAND> .... */
+    public static void main(String... args) {
+        if (args.length == 0) {
+            Utils.error("Please enter a command");
+        }
+
+    }
+
+
+
+}
